4bac3bda7a0a2fd3b281e83d4542b5ee495d818d
Elyson Vinicius <elysonvinicius@outlook.com>
2022-12-27 22:41:30 -0300

    feat: inserting selenium framework

---
+++ src/main/java/com/finchproject/estagio/config/SeleniumConfiguration.java
@@ -0,0 +1,37 @@
+package com.finchproject.estagio.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class SeleniumConfiguration {
+
+
+    @Bean
+    public WebDriver webDriver() {
+
+        // Inicialize o Selenium aqui, por exemplo:
+        System.setProperty("webdriver.chrome.driver", "src\\drive\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.setHeadless(true);
+
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+
+        // new ChromeOptions().setHeadless(true)
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
+}
