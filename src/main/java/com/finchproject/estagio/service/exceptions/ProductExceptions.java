125ceb612b1049aefde6bc72e8a484834d9c6c00
Elyson Vinicius <elysonvinicius@outlook.com>
2022-12-27 23:03:36 -0300

    feat: creating custom exception for product

---
+++ src/main/java/com/finchproject/estagio/service/exceptions/ProductExceptions.java
@@ -0,0 +1,8 @@
+package com.finchproject.estagio.service.exceptions;
+
+public class ProductExceptions extends RuntimeException{
+
+    public ProductExceptions(String msg){
+        super(msg);
+    }
+}
